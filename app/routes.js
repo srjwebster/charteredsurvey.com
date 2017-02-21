93fcabcd80ec24b2208b5d16085dc7aace34f506
Samuel Webster <sam@zeus.manyhatsdigital.com>
2017-02-21 14:36:59 +0000

    Initial commit

---
+++ app/routes.js
@@ -0,0 +1,36 @@
+// app/routes.js
+
+// grab the nerd model we just created
+var Nerd = require('./models/nerd');
+
+    module.exports = function(app) {
+
+        // server routes ===========================================================
+        // handle things like api calls
+        // authentication routes
+
+        // route to handle retrieving goes here.
+        app.get('/api/nerds', function(req, res) {
+            // use mongoose to get all nerds in the database
+            Nerd.find(function(err, nerds) {
+
+                // if there is an error retrieving, send the error. 
+                                // nothing after res.send(err) will execute
+                if (err)
+                    res.send(err);
+
+                res.json(nerds); // return all nerds in JSON format
+            });
+        });
+
+        // route to handle creating goes here (app.post)
+        // route to handle updating goes here (app.update)
+	// route to handle deleting goes here (app.delete)
+
+        // frontend routes =========================================================
+        // route to handle all angular requests
+        app.get('*', function(req, res) {
+            res.sendfile('./public/views/index.html'); // load our public/index.html file
+        });
+
+    };
